b89d97164d995e8a9328aaac4157b0cb24254cab
Gabriella Dauber <gabrielladauber@Gabriellas-MacBook-Pro.local>
2021-10-29 12:43:08 -0700

    Message

---
+++ 1.5.R
@@ -0,0 +1,19 @@
+# Gabriella Dauber, section 02, 10/10/2021, Assignment 1.5
+
+# Question 1
+# There are 8 entries. 
+
+# Question 2 
+a <- 7.15*10^(-4)
+
+# Question 3 
+log(745, base=5)
+
+# Question 4
+mean(c(4.56, 24.92, 8.28, 2.22, 9.46))
+
+# Question 5
+a < - 5
+#The error is that there is a space in between the arrow and dash.
+#To fix it, there needs to be no space so that the arrow is pointing to the a.
+
